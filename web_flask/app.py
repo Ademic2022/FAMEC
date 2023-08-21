40e54bc25216c86fe8552ad72bc7fad018e971d5
“Ademic” <adeeyomichael77@gmail.com>
2023-08-20 22:39:08 -0700

    new file:   api/__pycache__/__init__.cpython-39.pyc 	new file:   api/v1/__pycache__/__init__.cpython-39.pyc 	new file:   api/v1/views/__pycache__/__init__.cpython-39.pyc 	modified:   web_flask/__pycache__/app.cpython-39.pyc 	modified:   web_flask/app.py 	new file:   web_flask/static/scripts/login.js 	new file:   web_flask/static/scripts/register.js 	new file:   web_flask/static/styles/login.css 	new file:   web_flask/static/styles/register.css 	new file:   web_flask/templates/login.html 	new file:   web_flask/templates/register.html

--- web_flask/app.py
+++ web_flask/app.py
@@ -8,9 +8,21 @@ def hello_world():
     background_image_urls = [
         url_for('static', filename='images/home1.jpg'),
         url_for('static', filename='images/home2.jpg'),
-        url_for('static', filename='images/home3.jpg')
+        url_for('static', filename='images/home3.jpg'),
+        url_for('static', filename='images/hma_banner.jpg'),
+        url_for('static', filename='images/hma3.jpg'),
+        url_for('static', filename='images/hma1.jpg')
     ]
     return render_template('landing_page.html', images = background_image_urls)
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
