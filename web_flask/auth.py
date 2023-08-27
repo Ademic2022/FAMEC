abafa757e3ae4eb72e40715004690e9e78c9dc34
Ademic2022 <adeeyomichael77@gmail.com>
2023-08-27 14:59:13 +0100

    modified:   web_flask/auth.py 	renamed:    web_flask/static/scripts/login.js -> web_flask/static/scripts/header.js 	modified:   web_flask/static/styles/base.css 	renamed:    web_flask/static/styles/login.css -> web_flask/static/styles/header.css 	modified:   web_flask/static/styles/register.css 	new file:   web_flask/templates/auth/header.html 	renamed:    web_flask/templates/login.html -> web_flask/templates/auth/login.html 	new file:   web_flask/templates/auth/register.html 	deleted:    web_flask/templates/register.html

--- web_flask/auth.py
+++ web_flask/auth.py
@@ -5,11 +5,11 @@ auth = Blueprint('auth', __name__)
 
 @auth.route('/login')
 def login():
-    return render_template('login.html')
+    return render_template('auth/login.html')
 
 @auth.route('/register')
 def register():
-    return render_template('register.html')
+    return render_template('auth/register.html')
 
 @auth.route('/logout')
 def logout():
