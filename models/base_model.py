ca7a336c39e509fec0cb52be1b5e78deccafb863
Fatimah <fattyhassan14@gmail.com>
2023-08-29 15:24:20 -0400

    user model updated

--- models/base_model.py
+++ models/base_model.py
@@ -7,15 +7,15 @@ from sqlalchemy import Column, Integer, String, DateTime
 
 time = "%Y-%m-%dT%H:%M:%S.%f"
 
-if models.storage_t == "db":
-    Base = declarative_base()
-else:
-    Base = object
-    
+# if models.storage_t == "db":
+#     Base = declarative_base()
+# else:
+#     Base = object
+Base = declarative_base() 
 class BaseModel:
     """ The basemodel class that all other classes will inherit from """
-    if models.storage_t == "db":
-        id = Column(String(60), primary_key=True)
-        created_at = Column(DateTime, default=datetime.utc.now)
-        updated_at = Column(DateTime, default=datetime.utc.now)
+    # if models.storage_t == "db":
+    id = Column(String(60), primary_key=True, nullable=False, unique=True)
+    created_at = Column(DateTime, default=datetime.utcnow)
+    updated_at = Column(DateTime, default=datetime.utcnow)
         
