a605ae0a05f57e8e0c14b0324a5bce7da37f86b4
Fatimah <fattyhassan14@gmail.com>
2023-08-29 11:55:43 -0400

    New updates

--- sql_queries/create_user.sql
+++ sql_queries/create_user.sql
@@ -1,6 +1,6 @@
 -- a scripts that creates a user to our database .
--- using famec_01@localhost as the user
--- using famec_01_pwd
--- database name is famec_db
+-- using FAMEC_MYSQL_USER@localhost as the user
+-- using FAMEC_MYSQL_PWD
+-- database name is FAMEC_DB
 CREATE USER IF NOT EXISTS FAMEC_MYSQL_USER@localhost IDENTIFIED BY 'FAMEC_MYSQL_PWD';
 GRANT ALL PRIVILEGES ON *.* TO FAMEC_MYSQL_USER@localhost;
