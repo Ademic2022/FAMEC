fb0ec9d74c40362783358a7344edb9a7ea8e6122
ademic2022 <adeeyomichael@gmail.com>
2023-08-30 18:10:10 +0100

    modified:   .gitignore 	new file:   note.txt 	modified:   web_flask/__init__.py 	modified:   web_flask/auth.py 	modified:   web_flask/static/scripts/register.js 	modified:   web_flask/static/styles/header.css 	modified:   web_flask/templates/auth/header.html 	modified:   web_flask/templates/auth/login.html 	modified:   web_flask/templates/auth/register.html

--- web_flask/__init__.py
+++ web_flask/__init__.py
@@ -1,8 +1,12 @@
 from flask import Flask
+from flask_login import LoginManager
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'famec'
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
 
     from .views import views
     from .auth import auth
--- web_flask/auth.py
+++ web_flask/auth.py
@@ -1,7 +1,9 @@
 from flask import Blueprint, render_template, url_for,request, flash, redirect
+from flask_login import login_required, current_user
 from models import storage
 from models.user import User
 import hashlib
+import re
 
 auth = Blueprint('auth', __name__)
 
@@ -29,13 +31,43 @@ def login():
 # REGISTER ROUTE FUNCTIONALITY
 @auth.route('/register', methods=['GET', 'POST'])
 def register():
-    # firstname = 
-    # lastname = 
-    # username = 
-    # password = 
-    # confirm_pass = 
-    # email = 
+    if request.method == 'POST':
+        # action = request.form.get('register')
+        # if action == 'register':
+        user_data = {
+            "firstname": request.form.get('firstname'),
+            "lastname": request.form.get('lastname'),
+            "username": request.form.get('username'),
+            "password": request.form.get('password'),
+            "confirm_pass": request.form.get('confirm_pass'),
+            "email": request.form.get('email'),
+            "address": request.form.get('apt'),
+            "apt": request.form.get('apt'),
+            "country": request.form.get('country'),
+            "state": request.form.get('state'),
+            "birth_month": request.form.get('birth_month'),
+            "birth_day": request.form.get('birth_day'),
+            "t_c": request.form.get('t_c')
+        }
 
+        if len(user_data['firstname']) < 2:
+            flash('First Name must be greater than 1 character', category='error')
+        elif len(user_data['lastname']) < 2:
+            flash('Last Name must be greater than 1 character', category='error')
+        elif user_data['password'] != user_data['confirm_pass']:
+            flash('Passwords do not match', category='error')
+        elif len(user_data['password']) < 6:
+            flash('Password must be at least 6 characters long', category='error')
+        elif not re.match(r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$", user_data['email']):
+            flash('Invalid email address', category='error')
+        elif 't_c' not in user_data:
+            flash('You must accept the terms and conditions to register', category='error')
+        elif any(not user_data[field] for field in ['firstname', 'lastname', 'password', 'confirm_pass', 'email', 'address', 'apt', 'country', 'state', 'birth_month', 'birth_day']):
+            flash('All fields are required', category='error')
+        else:
+            # All checks passed, proceed with registration
+            flash('Registration successful', category='success')
+            return redirect(url_for('auth.login'))
 
     return render_template('auth/register.html')
 
