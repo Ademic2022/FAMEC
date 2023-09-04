7a0ac493ac4c66439e49413383299df36222721b
ademic2022 <adeeyomichael@gmail.com>
2023-09-04 23:42:52 +0100

    new file:   web_flask/static/images/family.jpg 	modified:   web_flask/static/styles/landing_page.css 	modified:   web_flask/templates/landing_page.html 	modified:   web_flask/views.py

--- web_flask/views.py
+++ web_flask/views.py
@@ -22,7 +22,8 @@ def landing_page():
         url_for('static', filename='images/famec_landn_1.jpg'),
         url_for('static', filename='images/Famec_logo_white.png'),
         url_for('static', filename='images/famec_landn_2.jpg'),
-        url_for('static', filename='images/famec_landn_3.jpg')
+        url_for('static', filename='images/famec_landn_3.jpg'),
+        url_for('static', filename='images/family.jpg')
     ]
     return render_template('landing_page.html', images = background_image_urls)
 
